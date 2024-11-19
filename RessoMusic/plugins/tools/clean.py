6b8a4cd2613f9fb237623e7e5acb9bd8d31cbb8f
dghfhdgbxgbg <134328176+dghfhdgbxgbg@users.noreply.github.com>
2024-11-19 23:50:26 +0530

    Create clean.py

---
+++ RessoMusic/plugins/tools/clean.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+from pyrogram import filters
+from RessoMusic import app
+from RessoMusic.misc import SUDOERS
+
+
+@app.on_message(filters.command("clean") & SUDOERS)
+async def clean(client, message):
+    A = await message.reply_text("ᴄʟᴇᴀɴɪɴɢ ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs...")
+    dir = "downloads"
+    dir1 = "cache"
+    shutil.rmtree(dir)
+    shutil.rmtree(dir1)
+    os.mkdir(dir)
+    os.mkdir(dir1)
+    await A.edit("ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs ᴀʀᴇ ᴄʟᴇᴀɴᴇᴅ")
