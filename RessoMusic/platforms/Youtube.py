1831f359e31b54365679cf01141e528423f5def4
NKMODNK6 <chotababbu654@gmail.com>
2024-12-04 02:18:30 +0530

    Update Youtube.py

--- RessoMusic/platforms/Youtube.py
+++ RessoMusic/platforms/Youtube.py
@@ -11,8 +11,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AviaxMusic.utils.database import is_on_off
-from AviaxMusic.utils.formatters import time_to_seconds
+from RessoMusic.utils.database import is_on_off
+from RessoMusic.utils.formatters import time_to_seconds
 
 
 
