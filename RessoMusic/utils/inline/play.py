f3dc6424a0f89750038bcbb10a3dd20ad806534a
PRIVATE-REPO4 <navnitjha49826@gmail.com>
2024-12-10 12:51:47 +0530

    Update play.py

--- RessoMusic/utils/inline/play.py
+++ RessoMusic/utils/inline/play.py
@@ -2,7 +2,7 @@ import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from AviaxMusic.utils.formatters import time_to_seconds
+from RessoMusic.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
