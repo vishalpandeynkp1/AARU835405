d16d1463414ad19d9fd74d7f972ec2c023d5ed60
PRIVATE-REPO4 <navnitjha49826@gmail.com>
2024-12-10 12:24:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/PRIVATE-REPO4/Personal-",
+    "https://github.com/PRIVATE-REPO4/STRINGBOT",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
