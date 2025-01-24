0f8826a5f6f8f43d4468974c48dfbf97af06a9a8
MOONl <mypersonalheruko@gmail.com>
2025-01-25 00:33:49 +0530

    Update mustjoin.py

--- RessoMusic/plugins/bot/mustjoin.py
+++ RessoMusic/plugins/bot/mustjoin.py
@@ -2,7 +2,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from PROMUSIC import app
+from RessoMusic import app
 
 #--------------------------
 
