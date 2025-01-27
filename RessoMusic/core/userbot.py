2ff29df95dfd7eac3cdb5d2cc437a4da5d0f0856
Rocky9852 <abhisheklkr628@gmail.com>
2025-01-27 20:04:19 +0530

    Update userbot.py

--- RessoMusic/core/userbot.py
+++ RessoMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("untold_coder")
-                await self.one.join_chat("Youtube-_Offical_Network")
+                await self.one.join_chat("SaregamaSupport")
+                await self.one.join_chat("SaregamaSupport")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.one.join_chat("untold_coder")
-                await self.one.join_chat("Youtube-_Offical_Network")
+                await self.one.join_chat("SaregamaSupport")
+                await self.one.join_chat("SaregamaSupport")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.one.join_chat("untold_coder")
-                await self.one.join_chat("Youtube-_Offical_Network")
+                await self.one.join_chat("SaregamaSupport")
+                await self.one.join_chat("SaregamaSupport")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.one.join_chat("untold_coder")
-                await self.one.join_chat("Youtube-_Offical_Network")
+                await self.one.join_chat("SaregamaSupport")
+                await self.one.join_chat("SaregamaSupport")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.one.join_chat("untold_coder")
-                await self.one.join_chat("Youtube-_Offical_Network")
+                await self.one.join_chat("SaregamaSupport")
+                await self.one.join_chat("SaregamaSupport")
             except:
                 pass
             assistants.append(5)
